c59271cdbf881bc1cbfac4861b766bdfd7219f46
Jacob Botimer <jake@memryx.com>
2019-04-20 10:04:06 -0400

    Incremental

--- src/game.cpp
+++ src/game.cpp
@@ -10,29 +10,74 @@ Game::Game(){
     }
 
 
-    //std::string default_pos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    std::string default_pos("2kr2nr/1pp1p2p/p3Pppb/3p4/3P1B2/5NP1/PP2QP1P/nN3K1R w - - 1 15");
+    std::string default_pos("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    //std::string default_pos("2kr2nr/1pp1p2p/p3Pppb/3p4/3P1B2/5NP1/PP2QP1P/nN3K1R w - - 1 15");
 
     set_board(default_pos);
 }
 
 void Game::set_board(std::string str_in){
-    std::string fen_pos = preprocess(str_in);
+    //std::string fen_pos = preprocess(str_in);
+    fen_to_gs(str_in);
     //if(fen_pos.length() != 64){
     //    std::cout << "bad fen string" << std::endl;
     //    return;
     //}
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            bstate[j][i] = char_to_ts[fen_pos[i*8 + j]];
-        }
-    }
 }
 
 
-std::string Game::preprocess(std::string fen){
-    // Take only the board part of FEN string 
-    std::string str = fen.substr(0,fen.find(" "));
+void Game::fen_to_gs(std::string fen){
+
+    // Parse fen string into components
+    std::vector<std::string> string_list;
+    std::size_t lower = 0;
+    std::size_t upper = 0;
+    while(lower != std::string::npos){
+        std::string tmp;
+        if(lower == 0){
+            tmp = fen.substr(lower,std::string::npos);
+        }else{
+            tmp = fen.substr(lower+1,std::string::npos);
+        }
+        upper = tmp.find(" ");
+        std::string new_string = fen.substr(lower+1, upper);
+        string_list.push_back(new_string);
+
+        fen = tmp;
+        lower = upper;
+    }
+
+    // Set turn
+    turn = "w" == string_list[1];
+
+    // Set castling
+    w_castle = 0;
+    b_castle = 0;
+
+    if(string_list[2].find("K") != std::string::npos){
+        w_castle = 2;
+    }
+    if(string_list[2].find("Q") != std::string::npos){
+        w_castle += 1;
+    }
+    if(string_list[2].find("k") != std::string::npos){
+        b_castle = 2;
+    }
+    if(string_list[2].find("q") != std::string::npos){
+        b_castle += 1;
+    }
+
+    // Set en passant 
+    en_passant = string_list[3];
+
+    // Set fifty move rule and number of turns
+    fifty_move_rule = std::stoi(string_list[4], nullptr, 10);
+    total_moves = std::stoi(string_list[5], nullptr, 10);
+
+    // Set board state
+    //std::string str = fen.substr(0,fen.find(" "));
+    std::string str = string_list[0];
+
     // Convert all numbers to a series of '-' 
     std::size_t found = str.find_first_of("12345678");
     while(found !=std::string::npos){
@@ -47,6 +92,19 @@ std::string Game::preprocess(std::string fen){
         str.erase(found,1);
         found = str.find_first_of("/", found);
     }
-    return str;
+
+    // Error checking
+    if(str.length() != 64){
+        std::cout << "bad fen string" << std::endl;
+        return;
+    }
+
+    // Convert to bstate
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            //bstate[j][i] = char_to_ts[fen_pos[i*8 + j]];
+            bstate[j][i] = char_to_ts[str[i*8 + j]];
+        }
+    }
 }
 
--- src/game.h
+++ src/game.h
@@ -2,6 +2,7 @@
 #define GAME_H
 
 #include <string> 
+#include <vector> 
 #include <map>
 
 #include <board.h>
@@ -16,18 +17,20 @@ public:
     // gamestate variables
     TS bstate[8][8];         
     bool turn;              // 0 - white; 1 - black
-    int  w_castle;          // 2'b11 => kq 
+    int  w_castle;          // two-hot encoded: 2'b11 => kq 
     int  b_castle;
+    std::string en_passant;
     int  fifty_move_rule;
     int  total_moves;
     // Some way to hold 3 move repeat // 
 
+    // 
 
 
     Game();
     void set_board(std::string str_in);
     //std::string fen_to_gs(std::string fen);
-    std::string preprocess(std::string fen);
+    void fen_to_gs(std::string fen);
 
 };
 
